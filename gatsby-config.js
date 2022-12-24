e94e6a12ae4931570a7b4ea7202d668dc2d0477c
designer-carlyn <carlyn.designer@gmail.com>
2022-12-24 21:44:08 +0800

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `COC Account Tracker`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-sass"]
+};
