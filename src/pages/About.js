247be2042b2caf9ba93acedd58e9fb4086f5b9f7
mpotech-segovia <carlosegovia_mktg@mpotech.net>
2022-12-28 10:36:53 +0800

    update home and add about

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <main>
+      <h1>About Me</h1>
+      <p>
+        Hi there! I'm the proud creator of this site, which I built with Gatsby.
+      </p>
+    </main>
+  );
+};
+
+export default About;
