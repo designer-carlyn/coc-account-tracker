7d81a98afca8caab85b254a595c1a470e4d99453
mpotech-segovia <carlosegovia_mktg@mpotech.net>
2023-01-03 11:25:58 +0800

    added components and props

---
+++ src/components/layout.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "gatsby";
+import "./layout.scss";
+
+const Layout = ({ pageTitle, children }) => {
+  return (
+    <div className="container">
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+      <main>
+        <h1>{pageTitle}</h1>
+        {children}
+      </main>
+    </div>
+  );
+};
+
+export default Layout;
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,14 +1,16 @@
 import React from "react";
+import Layout from "../components/layout";
 
 const About = () => {
   return (
-    <main>
-      <h1>About Me</h1>
+    <Layout pageTitle="About Me">
       <p>
         Hi there! I'm the proud creator of this site, which I built with Gatsby.
       </p>
-    </main>
+    </Layout>
   );
 };
+
+export const Head = () => <title>About Me</title>;
 
 export default About;
