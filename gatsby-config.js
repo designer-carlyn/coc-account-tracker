36e8f62c0e5232e42a0c7c31c9ba4600b66ea41c
mpotech-segovia <carlosegovia_mktg@mpotech.net>
2023-01-03 11:39:24 +0800

    added static image

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,20 @@ module.exports = {
     title: `COC Account Tracker`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: ["gatsby-plugin-sass"],
+  plugins: [
+    "gatsby-plugin-sass",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    {
+      resolve: `gatsby-plugin-emotion`,
+      options: {
+        // Accepts the following options, all of which are defined by `@emotion/babel-plugin` plugin.
+        // The values for each key in this example are the defaults the plugin uses.
+        sourceMap: true,
+        autoLabel: "dev-only",
+        labelFormat: `[local]`,
+        cssPropOptimization: true,
+      },
+    },
+  ],
 };
