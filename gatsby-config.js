4cab4ad955aea05d78fd4bb548b6cbf0ab6da069
mpotech-segovia <carlosegovia_mktg@mpotech.net>
2023-01-06 12:46:52 +0800

    add netlify plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,7 @@ module.exports = {
     siteUrl: `https://www.yourdomain.tld`,
   },
   plugins: [
+    "gatsby-plugin-netlify",
     "gatsby-plugin-sass",
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
